ed0f2a8c8bda60626ae31402f7385ad5618d8d9d
Paul Battley <pbattley@gmail.com>
2012-08-15 10:16:17 +0100

    Working extension

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+  if (tab.url.match(/^https?:\/\/(www\.)?dailymail\.co\.uk/.+/)) {
+    chrome.tabs.update(tabId, {
+      url: 'http://www.instapaper.com/text?u=' + encodeURIComponent(tab.url)
+    });
+  }
+});
