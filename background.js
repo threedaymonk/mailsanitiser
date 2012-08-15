1f822e9b9b921186cf12fedb306e0d40b5dfe20a
Paul Battley <pbattley@gmail.com>
2012-08-15 11:32:59 +0100

    Fix regexp, and allow on index page

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-  if (tab.url.match(/^https?:\/\/(www\.)?dailymail\.co\.uk/.+/)) {
+  if (tab.url.match(/^https?:\/\/(www\.)?dailymail\.co\.uk/)) {
     chrome.tabs.update(tabId, {
       url: 'http://www.instapaper.com/text?u=' + encodeURIComponent(tab.url)
     });
